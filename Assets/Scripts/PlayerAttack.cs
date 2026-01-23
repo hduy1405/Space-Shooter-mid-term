7a84871485e5610b4c6b20275fd089cb77719c02
Nguyen Duy <duynguyen14051005@gmail.com>
2026-01-23 10:06:51 +0700

    demo2

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -1,20 +1,15 @@
 using UnityEngine;
 
-public class PlayerAttack : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
-    public Transform firePoint;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
-            Shoot();
+        Instantiate(bulletPrefab, transform.position,
+        transform.rotation);
         }
-    }
-
-    void Shoot()
-    {
-        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
     }
 }
